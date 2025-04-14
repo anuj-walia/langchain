82d4b69580afd9935275095445a62358cfb12cff
anuj-walia <anuj_walia@protonmail.com>
2025-04-13 20:59:58 -0400

    Recovered files 3

---
+++ init_environment.py
@@ -0,0 +1,7 @@
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAI
+load_dotenv('conf/.env',override=True)
+
+
